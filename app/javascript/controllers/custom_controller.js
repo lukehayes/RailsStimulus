f92ac5ac092ae3d5b8201c1cc80b9a11cf20987d
lukehayes <lukepurplehayes@gmail.com>
2024-10-28 11:23:46 +0000

    Added start of controller action targets

--- app/javascript/controllers/custom_controller.js
+++ app/javascript/controllers/custom_controller.js
@@ -1,11 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = ['name'];
+
   connect() {
-    this.element.textContent = "A custom controller right here."
+    //this.element.textContent = "A custom controller right here."
   }
 
   foo() {
     console.log("Element has been clicked");
+    const name = this.nameTarget;
+    console.log(name);
+    console.log(name.value);
   }
 }
